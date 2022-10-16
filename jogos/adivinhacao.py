1cb12c5f4912c8fb0a75936f7ac6291162acfbcd
Yure França <yure.ads@gmail.com>
2022-10-16 07:37:38 -0300

    v161022.1

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -4,22 +4,32 @@ print("*********************************")
 
 numero_secreto = 42
 
-chute_str = input("Digite o seu número: ")
+total_tentativas = 3
+rodada = 1
 
-print("Você digitou ", chute_str)
 
-chute = int(chute_str)
+for rodada in range(1, total_tentativas +1):
+    print("Tentativa {} de {}".format(rodada, total_tentativas))
+    chute_str = input("Digite o seu número entre 1 e 100: ")
+    print("Você digitou ", chute_str)
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
 
-if (acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior que o número secreto.")
-    elif(menor):
-        print("Você errou! O seu chute foi menor que o número secreto.")
+    chute = int(chute_str)
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou!")
+        break
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
 
 print("Fim do jogo!")
