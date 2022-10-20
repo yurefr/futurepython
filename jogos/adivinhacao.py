6c0884e658557fc2544604b80dc1d78358f4f0c6
Yure França <yure.ads@gmail.com>
2022-10-20 06:10:02 -0300

    v.20102022.1

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -5,8 +5,21 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
 numero_secreto = random.randrange(1,101)
-total_tentativas = 3
+total_tentativas = 0
 rodada = 1
+pontos = 1000
+
+print("Escolha um nível de dificuldade:")
+print("1 - Fácil (20) | 2 - Médio (10) | 3 Difícil (5)")
+
+nivel = int(input("Define o nível: "))
+
+if  (nivel == 1):
+    total_tentativas = 20
+elif(nivel == 2):
+    total_tentativas = 10
+else:
+    total_tentativas = 5
 
 for rodada in range(1, total_tentativas +1):
     print("Tentativa {} de {}".format(rodada, total_tentativas))
@@ -24,12 +37,14 @@ for rodada in range(1, total_tentativas +1):
     menor   = chute < numero_secreto
 
     if (acertou):
-        print("Você acertou!")
+        print("Você acertou e fez {} pontos!".format(pontos))
         break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif(menor):
             print("Você errou! O seu chute foi menor que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 
 print("Fim do jogo!")
